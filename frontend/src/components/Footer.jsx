ddc147ee48705c40f45f3700f8f0f75574ba5793
Harshith Rao <harshithrao316@gmail.com>
2023-08-30 17:45:04 +0530

    changed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return(
-        <footer style={{ cursor:"auto"}}>
-            <p>Made with ❤️ by <a href="https://github.com/harshithrao07" style={{textDecoration:"none", cursor:"pointer"}}>Harshith Rao</a></p>
+        <footer style={{ cursor:"grab"}}>
+            <p>Made with ❤️ by <a href="https://github.com/harshithrao07" style={{textDecoration:"none", cursor:"grabbing"}}>Harshith Rao</a></p>
         </footer>
     )
 }
